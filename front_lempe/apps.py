3064006f4b24dcdd8b2bed37cb1c5a1609e201ce
ThalisFernandes <thalisfernandesrodrigues@gmail.com>
2022-12-14 00:58:56 -0300

    feat: subindo os primeiros arquivos do projeto

---
+++ front_lempe/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FrontLempeConfig(AppConfig):
+    name = 'front_lempe'
---
+++ front_lempe/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def renderpage():
+    
+    return HttpResponse("Hello, world. You're at the polls index.")
